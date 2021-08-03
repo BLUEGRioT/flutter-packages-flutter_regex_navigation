7e73f3ce0ba7c42ebf49d3e2ed3cad5a818487de
Florent ROBIN <frobin@bluegriot.com>
2021-08-03 08:42:43 +0200

    add cupertino page route

--- lib/src/routing.dart
+++ lib/src/routing.dart
@@ -1,9 +1,19 @@
 import 'dart:async';
 import 'dart:collection';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
+enum ThemeType {
+  cupertino, 
+  material
+}
+
+typedef ModalRouteBuilder = Route? Function(ThemeType, AppPage);
+
 class NavigationStateRouter extends InheritedWidget {
+  final ThemeType themeType;
+  final ModalRouteBuilder? _modalRouteBuilder;
   final NavigationState _defaultNavigationState;
   final NavigationState Function(Uri uri)? _notFoundNavigationState;
   final NavigationStateRouteBuilder _navigationStateRouteBuilder;
@@ -12,11 +22,14 @@ class NavigationStateRouter extends InheritedWidget {
     required NavigationStateRouteBuilder navigationStateRouteBuilder,
     required NavigationState defaultNavigationState, 
     NavigationState Function(Uri uri)? notFoundNavigationState, 
+    this.themeType = ThemeType.material,
+    ModalRouteBuilder? modalRouteBuilder,
     required Widget child, 
   }) : 
     _navigationStateRouteBuilder = navigationStateRouteBuilder,
     _defaultNavigationState = defaultNavigationState, 
     _notFoundNavigationState = notFoundNavigationState,
+    _modalRouteBuilder = modalRouteBuilder,
     super(child: child) {
       _NavigationManager.shared.registerRoutes(_navigationStateRouteBuilder.routes);
     }
@@ -39,6 +52,8 @@ class NavigationStateRouter extends InheritedWidget {
   RouterDelegate<NavigationState> get routerDelegate => _routerDelegate;
 
   void push(NavigationState state) => _routerDelegate.pushNavigationState(state);
+
+  NavigationState? pop<T>([T? result]) => _routerDelegate.popNavigationState(result);
 }
 
 extension NavigationStateRouterExtension on BuildContext {
@@ -65,6 +80,9 @@ class _NavigationManager {
   _NavigationManager._internal();
   static _NavigationManager get shared => _singleton;
 
+  List<NavigationState> navigationStateStack = [];
+  Map<String, AppPage> pagesCache = {};
+
   final LinkedHashMap<String, NavigationStateBuilder> _routePatterns = LinkedHashMap<String, NavigationStateBuilder>();
 
   void registerRoute(NavigationStateRoute route) {
@@ -84,7 +102,13 @@ class _NavigationManager {
 abstract class NavigationState {
   final bool animate;
   String get path;
-  AppPage getPage(BuildContext context);
+  AppPage buildPage(BuildContext context);
+  AppPage _getPage(BuildContext context) {
+    if (!_NavigationManager.shared.pagesCache.containsKey(this.path)) {
+      _NavigationManager.shared.pagesCache[this.path] = buildPage(context);
+    }
+    return _NavigationManager.shared.pagesCache[this.path]!;
+  }
 
   NavigationState({this.animate = true});
 }
@@ -98,7 +122,7 @@ class _NotFoundNavigationState extends NavigationState {
   String get path => uri.path;
 
   @override
-  AppPage getPage(BuildContext context) => AppPage(state: this, child: Scaffold(body: Center(child: Text("Page not found"))));
+  AppPage buildPage(BuildContext context) => AppPage(state: this, child: Scaffold(body: Center(child: Text("Page not found"))));
 }
 
 
@@ -143,8 +167,6 @@ class _AppRouteDelegate extends RouterDelegate<NavigationState> with ChangeNotif
 
   NavigationState? _currentState;
 
-  List<NavigationState> _navigationStateStack = [];
-
   _AppRouteDelegate(this._defaultNavigationState);
 
   @override
@@ -153,8 +175,13 @@ class _AppRouteDelegate extends RouterDelegate<NavigationState> with ChangeNotif
   }
 
   void pushNavigationState(NavigationState state) {
-    _navigationStateStack.add(currentConfiguration);
+    _NavigationManager.shared.navigationStateStack.add(currentConfiguration);
     setNewRoutePath(state);
+  }
+
+  NavigationState? popNavigationState<T>([T? result]) {
+    return _NavigationManager.shared.navigationStateStack.last;
+    navigatorKey.currentState?.pop(result);
   }
 
   @override
@@ -163,22 +190,24 @@ class _AppRouteDelegate extends RouterDelegate<NavigationState> with ChangeNotif
     final configuration = currentConfiguration;
 
     // Add previous pages in the history
-    for (var item in _navigationStateStack) {
-      pages.add(item.getPage(context));
+    for (var item in _NavigationManager.shared.navigationStateStack) {
+      pages.add(item._getPage(context));
     }
     
 
     // Add current page
-    pages.add(configuration.getPage(context));
+    pages.add(configuration._getPage(context));
     
     return Navigator(
+      key: navigatorKey,
       pages: pages,
       onPopPage: (_, __) {
         if (!_.didPop(__)) {
           return false;
         }
         pages.removeLast();
-        _navigationStateStack.removeLast();
+        final oldState = _NavigationManager.shared.navigationStateStack.removeLast();
+        _NavigationManager.shared.pagesCache.remove(oldState.path);
         final page = pages.last;
         if (page is AppPage) {
           _currentState = page.state;
@@ -208,7 +237,21 @@ class AppPage<T> extends MaterialPage<T> {
 
   @override
   Route<T> createRoute(BuildContext context) {
-    return _AppPageBasedMaterialPageRoute(page: this);
+    dynamic result;
+    ModalRouteBuilder? modalRouteBuilder = NavigationStateRouter.of(context)._modalRouteBuilder;
+    if (this.fullscreenDialog && modalRouteBuilder != null) {
+      result = modalRouteBuilder(NavigationStateRouter.of(context).themeType, this);
+    }
+    if (result != null) {
+      return result;
+    }
+    switch (NavigationStateRouter.of(context).themeType) {
+      case ThemeType.material:
+        return _AppPageBasedMaterialPageRoute(page: this);   
+      case ThemeType.cupertino:
+        return _AppPageBasedCupertinoPageRoute(page: this);   
+    }
+    throw Exception("Unsupported type");
   }
 }
 
@@ -225,6 +268,36 @@ class _AppPageBasedMaterialPageRoute<T> extends PageRoute<T> with MaterialRouteT
   Widget buildContent(BuildContext context) {
     return _page.child;
   }
+
+  @override
+  Duration get transitionDuration => _page.state.animate ? super.transitionDuration : Duration.zero;
+
+  @override
+  bool get maintainState => _page.maintainState;
+
+  @override
+  bool get fullscreenDialog => _page.fullscreenDialog;
+
+  @override
+  String get debugLabel => '${super.debugLabel}(${_page.name})';
+}
+
+class _AppPageBasedCupertinoPageRoute<T> extends PageRoute<T> with CupertinoRouteTransitionMixin<T> {
+  _AppPageBasedCupertinoPageRoute({
+    required AppPage<T> page,
+  }) : super(settings: page) {
+    assert(opaque);
+  }
+
+  AppPage<T> get _page => settings as AppPage<T>;
+
+  @override
+  Widget buildContent(BuildContext context) {
+    return _page.child;
+  }
+
+  @override
+  String? get title => _page.name;
 
   @override
   Duration get transitionDuration => _page.state.animate ? super.transitionDuration : Duration.zero;
